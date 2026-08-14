0249b6c0107f22f116e8fce4799d569f33033e72
abdulalisadaqat <abdulalisadaqat@gmail.com>
2026-08-14 16:39:23 +0430

    fix: move prettierConfig before eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
   {
     rules: {
       "no-unused-expressions": "error",
@@ -14,7 +15,6 @@ const eslintConfig = defineConfig([
       "no-trailing-spaces": "error",
     },
   },
-  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
